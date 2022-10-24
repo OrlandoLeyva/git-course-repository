c01f352e5878803ae49e131ace23c8cf497dc97c
orlando leyva <leyvaorlandomx08@gmail.com>
2022-10-24 15:50:09 -0700

    res

--- secondModule/secondModule.js
+++ secondModule/secondModule.js
@@ -1,7 +1,11 @@
-console.log('secondModule');
+console.log("secondModule");
 
-function sum (num1, num2){
-    return num1 + num2;
+function sum(num1, num2) {
+  return num1 + num2;
 }
 
-module.exports = sum
+function res(num1, num2) {
+  return num1 - num2;
+}
+
+module.exports = sum;
